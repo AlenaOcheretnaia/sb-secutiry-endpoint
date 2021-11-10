d06d0323023edfc64bda3e9a3d469e2492fa3ad7
AlenaOcheretnaia <alenaocheretnaia@gmail.com>
2021-11-10 13:29:35 -0500

    Added Safe Methods Challenge

--- src/main/java/netology/aloch/SecurityConfig.java
+++ src/main/java/netology/aloch/SecurityConfig.java
@@ -2,23 +2,24 @@ package netology.aloch;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @Configuration
+@EnableGlobalMethodSecurity(securedEnabled=true, prePostEnabled=true, jsr250Enabled=true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication().withUser("alena").password("{noop}12345").roles("ALL")
-                .and().withUser("anela").password("{noop}54321").roles("READ");
+        auth.inMemoryAuthentication().withUser("userRead").password("{noop}12345").roles("READ")
+                .and().withUser("userWrite").password("{noop}12345").roles("WRITE")
+                .and().withUser("userReadDelete").password("{noop}12345").roles("READ","DELETE");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.formLogin().and()
-                .authorizeRequests().antMatchers("/hello").permitAll()
-                .and()
-                .authorizeRequests().anyRequest().authenticated();
+        http.formLogin()
+                .and().authorizeRequests().anyRequest().authenticated();
     }
 }
--- src/main/java/netology/aloch/controller/SecurityController.java
+++ src/main/java/netology/aloch/controller/SecurityController.java
@@ -1,27 +1,53 @@
 package netology.aloch.controller;
 
+import org.springframework.security.access.annotation.Secured;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.security.RolesAllowed;
 
 @RestController
 public class SecurityController {
 
-    //without authorzation
-    @GetMapping("/hello")
+    @GetMapping("/")
     public String helloPage() {
-        return ("Hello ALL from Security Application");
+        return ("Hello, " + SecurityContextHolder.getContext().getAuthentication().getName() + " from Security Application");
     }
 
-    //authorization needed
-    @GetMapping("/haverights")
-    public String restrictedPage() {
-        return ("Hello, Authorized user");
+    //только для пользователей с ролью "READ"(используйте @Secured)
+    @GetMapping("have-role-read")
+    @Secured("ROLE_READ")
+    public String checkRoleRead() {
+        return ("Hello, user with READ role");
     }
 
-    //authorization needed
-    @GetMapping("/authorize")
-    public String paramAuthorize(@RequestParam("user") String user, @RequestParam("password") String password) {
-        return ("Query Params are: <br> user = " + user + " , password = " + password);
+    //только для пользователей с ролью "WRITE"(используйте @RolesAllowed)
+    @GetMapping("have-role-write")
+    @RolesAllowed("ROLE_WRITE")
+    public String checkRoleWrite() {
+        return ("Hello, user with WRITE role");
     }
+
+    //если у пользователя есть хотя бы одна из ролей из "WRITE", "DELETE"(используйте pre/post аннотации)
+    @GetMapping ("have-role-wr-del")
+    @PreAuthorize("hasRole('WRITE') or hasRole('DELETE')")
+    public String checkWriteDeleteRole() {
+        return "User has WRITE or DELETE role";
+    }
+
+    //принимает в качестве query параметра имя пользователя(username),
+    //возвращает значения только если username совпадает с именем пользователя в вашем объекте Authentication,
+    //который Spring security сохраняет в SecurityContextHolder после успешной аутентификации
+    @GetMapping("/check-username")
+    @PostAuthorize("#username == authentication.principal.username")
+    public String paramUsername(@RequestParam("username") String username) {
+        return ("Hello user with name = " + username);
+    }
+
 }
